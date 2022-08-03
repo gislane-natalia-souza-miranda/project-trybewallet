6215e239c1ff5cefd0e6a1ff9d6cf7c35eed62d8
Gislane Natália de Souza Miranda <gislane.natalia.souza.miranda@gmail.com>
2022-08-03 15:55:09 -0300

    Req 6 - Desenvolva uma tabela com os gastos

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
+// import { Table } from 'react-bootstrap/Table';
 
 export default class Table extends Component {
   render() {
     return (
-      <div>Table</div>
+      <thead>
+        Comp Table
+        <tr>
+          <th>Descrição</th>
+          <th>Tag</th>
+          <th>Método de pagamento</th>
+          <th>Valor</th>
+          <th>Moeda</th>
+          <th>Câmbio utilizado</th>
+          <th>Valor convertido</th>
+          <th>Moeda de conversão</th>
+          <th>Editar/Excluir</th>
+        </tr>
+      </thead>
     );
   }
 }
---
+++ src/testRTL/Login.test.js
@@ -0,0 +1,13 @@
+/* import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import { About } from '../pages';
+
+describe('Testa o componente <About.js />', () => {
+  test('Teste se a página contém as informações sobre a Pokédex', () => {
+    renderWithRouter(<About />);
+
+    const textAbout = screen.getByText(/This application simulates/i);
+    expect(textAbout).toBeInTheDocument();
+  });
+}); */
